729747da0d13ebfeaf89c9cf679eaf0e7c58a8ce
fabferlin <fabferlin@gmail.com>
2021-01-19 17:23:28 -0300

    singleton java config

--- conference-java/src/main/java/AppConfig.java
+++ conference-java/src/main/java/AppConfig.java
@@ -2,8 +2,10 @@ import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 // bootstrap configuration | the start point of your app
@@ -12,6 +14,7 @@ public class AppConfig {
     // beans are by default a SINGLETON = will only
     // execute the method the first time it is called
     @Bean(name = "speakerService")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON) // it is a singleton
     public SpeakerService getSpeakerService() {
         SpeakerServiceImpl serviceImpl = new SpeakerServiceImpl(getSpeakerRepository());
         // serviceImpl.setRepository(getSpeakerRepository());
