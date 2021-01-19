0d76a0ab4ec7da68574aaeac2b77c037eda5548e
fabferlin <fabferlin@gmail.com>
2021-01-19 14:51:08 -0300

    without spring

---
+++ src/main/java/com/pluralsight/com/pluralsight/service/SpeakerServiceImpl.java
@@ -0,0 +1,19 @@
+package com.pluralsight.com.pluralsight.service;
+
+import com.pluralsight.model.Speaker;
+import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
+import com.pluralsight.repository.SpeakerRepository;
+
+import java.util.List;
+
+// impl business logic
+public class SpeakerServiceImpl implements SpeakerService {
+
+    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+
+    @Override
+    public List<Speaker> findAll() {
+        return repository.findall();
+    }
+
+}
